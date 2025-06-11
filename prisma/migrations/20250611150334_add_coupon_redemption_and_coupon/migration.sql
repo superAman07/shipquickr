44f4b0bab719be3c104880e788ecff79d9856fbf
superAman07 <rivalaman123@gmail.com>
2025-06-11 20:35:30 +0530

    added schema for coupon and coupon redemption

---
+++ prisma/migrations/20250611150334_add_coupon_redemption_and_coupon/migration.sql
@@ -0,0 +1,57 @@
+-- CreateEnum
+CREATE TYPE "CouponAmountType" AS ENUM ('fixed', 'percent');
+
+-- CreateTable
+CREATE TABLE "Coupon" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "endDate" TIMESTAMP(3) NOT NULL,
+    "limit" INTEGER NOT NULL,
+    "usedCount" INTEGER NOT NULL DEFAULT 0,
+    "condition" TEXT,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "amountType" "CouponAmountType" NOT NULL DEFAULT 'fixed',
+    "schedule" TIMESTAMP(3),
+    "status" BOOLEAN NOT NULL DEFAULT true,
+    "createdBy" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Coupon_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CouponRedemption" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "couponId" INTEGER NOT NULL,
+    "redeemedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "CouponRedemption_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Coupon_code_key" ON "Coupon"("code");
+
+-- CreateIndex
+CREATE INDEX "Coupon_code_idx" ON "Coupon"("code");
+
+-- CreateIndex
+CREATE INDEX "Coupon_status_idx" ON "Coupon"("status");
+
+-- CreateIndex
+CREATE INDEX "CouponRedemption_couponId_idx" ON "CouponRedemption"("couponId");
+
+-- CreateIndex
+CREATE INDEX "CouponRedemption_userId_idx" ON "CouponRedemption"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "CouponRedemption_userId_couponId_key" ON "CouponRedemption"("userId", "couponId");
+
+-- AddForeignKey
+ALTER TABLE "CouponRedemption" ADD CONSTRAINT "CouponRedemption_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CouponRedemption" ADD CONSTRAINT "CouponRedemption_couponId_fkey" FOREIGN KEY ("couponId") REFERENCES "Coupon"("id") ON DELETE CASCADE ON UPDATE CASCADE;
