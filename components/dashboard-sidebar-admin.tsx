a5bc47cde701d62a09dc6dc4081aa57d489a6945
superAman07 <rivalaman123@gmail.com>
2025-06-12 20:27:35 +0530

    added button on sidebar of admins dashboard

--- components/dashboard-sidebar-admin.tsx
+++ components/dashboard-sidebar-admin.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Package, Truck, Info, Calculator, ChevronRight, Ticket , Users, UserCheck, Plus, ListFilter, User } from "lucide-react";
+import { Package, Truck, Info, Calculator, ChevronRight, Ticket , Users, UserCheck, Plus, ListFilter, User, Newspaper } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
 import { useEffect, useState } from "react";
@@ -129,6 +129,13 @@ export default function DashboardSidebarAdmin() {
           label="Shipping Rates"
           href="/admin/dashboard/shipping-rates"
           active={pathname === "/admin/dashboard/shipping-rates"}
+          collapsed={sidebarCollapsed}
+        />  
+        <NavItem
+          icon={<Newspaper className="h-5 w-5" />}
+          label="News"
+          href="/admin/dashboard/news"
+          active={pathname === "/admin/dashboard/news"}
           collapsed={sidebarCollapsed}
         />  
       </nav>
