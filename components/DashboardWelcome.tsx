17e8630456da2dde289b2887d37c522418de961a
superAman07 <rivalaman123@gmail.com>
2025-07-09 17:59:33 +0530

    updated code by changing name color on dashboard welcome page

--- components/DashboardWelcome.tsx
+++ components/DashboardWelcome.tsx
@@ -6,7 +6,7 @@ type DashboardWelcomeProps = {
 export default function DashboardWelcome({ name, message }: DashboardWelcomeProps) {
   return (
     <div className="mb-4">
-      <h2 className="text-2xl text-[#495057] dark:text-gray-100 pl-2 md:pl-0 font-bold">Welcome, {name} 👋</h2>
+      <h2 className="text-2xl text-[#495057] dark:text-gray-100 pl-2 md:pl-0 font-bold">Welcome, <span className="text-[#389bde]">{name}</span>  👋</h2>
       {message && <p className="text-gray-500">{message}</p>}
     </div>
   );
