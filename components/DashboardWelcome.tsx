a43b7bbbc55c53baac9d3af6ea35dceac304034a
superAman07 <rivalaman123@gmail.com>
2025-07-10 16:44:55 +0530

    added beautiful bg till some height on dashboard

--- components/DashboardWelcome.tsx
+++ components/DashboardWelcome.tsx
@@ -5,8 +5,8 @@ type DashboardWelcomeProps = {
   
 export default function DashboardWelcome({ name, message }: DashboardWelcomeProps) {
   return (
-    <div className="mb-4">
-      <h2 className="text-2xl text-[#495057] dark:text-gray-100 pl-2 md:pl-0 font-bold">Welcome, <span className="text-[#389bde]">{name}</span>  👋</h2>
+    <div className="mb-4 pt-6 sm:pt-3">
+      <h2 className="text-xl sm:text-2xl text-gray-100 sm:text-[#495057] dark:text-gray-100 pl-2 md:pl-0 font-bold">Welcome, <span className="text-[#389bde]">{name}</span>  👋</h2>
       {message && <p className="text-gray-500">{message}</p>}
     </div>
   );
--- components/dashboard-layout-wrapper.tsx
+++ components/dashboard-layout-wrapper.tsx
@@ -24,6 +24,8 @@ export default function DashboardLayoutWrapper({ userRole, userName, children }:
                 setMobileMenuOpenAction={setMobileMenuOpen}
             />
             <div className="flex bg-gray-50 dark:bg-[#10162A] pt-16">
+                <div className="block sm:hidden w-full h-[180px] bg-gradient-to-r from-[#0a0c37] to-[#0a0c37] absolute left-0 top-0 z-0 pointer-events-none" />
+                <div className="relative flex flex-1"> 
                 <aside className="sticky top-16 h-[calc(100vh-4rem)] z-30">
                     {userRole === "admin" ? (
                         <DashboardSidebarAdmin />
@@ -36,6 +38,7 @@ export default function DashboardLayoutWrapper({ userRole, userName, children }:
                 </aside>
                 <div className="flex-1 flex flex-col min-w-0 w-full">
                     <main className="flex-1 p-3 sm:p-4 lg:p-6 overflow-hidden">{children}</main>
+                </div>
                 </div>
             </div>
         </>
