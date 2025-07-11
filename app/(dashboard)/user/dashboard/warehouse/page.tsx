cff7876028108658b72d3091e1a465efd344cf4d
superAman07 <rivalaman123@gmail.com>
2025-07-11 14:16:32 +0530

    fixed top header of warehouse page

--- app/(dashboard)/user/dashboard/warehouse/page.tsx
+++ app/(dashboard)/user/dashboard/warehouse/page.tsx
@@ -67,31 +67,31 @@ export default function WarehousesPage() {
 
   return (
     <>
-      <header className="dark:text-amber-50 rounded-2xl bg-gradient-to-r from-indigo-950 to-purple-900 px-2 py-2 shadow text-primary-foreground mb-4 md:mb-6 mx-2 md:mx-4">
+      <header className="dark:text-amber-50 rounded-2xl mt-10 sm:mt-0 bg-gradient-to-r from-white to-gray-50 px-2 py-2 shadow text-primary-foreground mb-4 md:mb-6 mx-2 md:mx-4">
         <div className="container mx-auto py-3 px-3 sm:py-4 sm:px-6">
           <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4">
             <div>
-              <div className="flex items-center gap-2 dark:text-amber-50">
+              <div className="flex items-center gap-2 text-gray-800 dark:text-amber-50">
                 <FileCheck className="h-5 w-5 sm:h-6 sm:w-6" />
                 <h1 className="text-xl sm:text-2xl dark:text-amber-50 font-bold tracking-tight">
                   {title}
                 </h1>
               </div>
               {subtitle && (
-                <p className="text-xs sm:text-sm text-primary-foreground/80 dark:text-amber-50/90">
+                <p className="text-xs sm:text-sm text-gray-800 dark:text-amber-50/90">
                   {subtitle}
                 </p>
               )}
-              <div className="mt-2 flex flex-wrap items-center gap-1 min-w-0 text-xs sm:text-sm text-primary-foreground/70 dark:text-amber-50/80">
+              <div className="mt-2 flex flex-wrap items-center gap-1 min-w-0 text-xs sm:text-sm text-gray-800 dark:text-amber-50/80">
                 <Link
                   href="/user/dashboard"
-                  className="flex items-center hover:text-gray-300 transition-colors min-w-0 shrink-0"
+                  className="flex items-center text-gray-800 hover:text-gray-600 transition-colors min-w-0 shrink-0"
                 >
                   <Home className="h-3 w-3 sm:h-3.5 sm:w-3.5 mr-1" />
                   <span className="truncate">Dashboard</span>
                 </Link>
                 <ChevronRight className="h-3 w-3 sm:h-3.5 sm:w-3.5 mx-1" />
-                <span className="font-medium truncate">Rate Calculator</span>
+                <span className="font-medium truncate">Warehouse</span>
               </div>
             </div>
             <button
