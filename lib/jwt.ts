9bdb524c4f8f402c702997f9c53a6bce8eb4c0da
superAman07 <rivalaman123@gmail.com>
2026-02-15 03:14:59 +0530

    increased the login lifetime

--- lib/jwt.ts
+++ lib/jwt.ts
@@ -1,13 +1,13 @@
 import jwt from "jsonwebtoken";
 
-const JWT_SECRET = process.env.JWT_SECRET!; 
+const JWT_SECRET = process.env.JWT_SECRET!;
 
 export function signToken(payload: object, p0: string) {
-  return jwt.sign(payload, JWT_SECRET, { expiresIn: "1h" });
+  return jwt.sign(payload, JWT_SECRET, { expiresIn: "48h" });
 }
 
 interface DecodedToken {
-  userId: string;   
+  userId: string;
   role?: string;
 }
 
