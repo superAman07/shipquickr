6d6031d375143ff5163573e83042c5b773451613
superAman07 <rivalaman123@gmail.com>
2026-03-14 11:57:23 +0530

    fixed the email sjndf

--- lib/email.ts
+++ lib/email.ts
@@ -8,6 +8,9 @@ export const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,  
     pass: process.env.EMAIL_PASS,  
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 const fromAddress = '"ShipQuickr" <updates@shipquickr.com>';
